e797fc6dcf961ecc9b9f9a0cded4a03f53de73f8
Lily Skye <me@suchipi.com>
2023-07-14 18:21:35 -0600

    update reference path

--- my_program.js
+++ my_program.js
@@ -1,4 +1,4 @@
-/// <reference path="./yavascript.d.ts" />
+/// <reference path="./deps/yavascript/yavascript.d.ts" />
 import * as std from "quickjs:std";
 
 console.log(inspect(std, { colours: true }));
